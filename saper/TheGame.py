c89bc4544a599057fdfb2af350fea6f9ad669520
deluti <74132604+deluti@users.noreply.github.com>
2022-01-23 19:26:45 +0700

    Add files via upload

--- saper/TheGame.py
+++ saper/TheGame.py
@@ -1,21 +1,52 @@
 import myLib
-myLib.vyvodPolya(myLib.vidimost_polya)
-game = True
+from myLib import prov
+from myLib import pole
 myLib.mins()
-myLib.prov()
+prov()
+game = True
 while game:
+    run = 0
+    run1 = False
+    run2 = False
     while True:
+        print('Что делаем?(1-флаг, 2-ход)')
+        run = input()
+        if run == '1':
+            run1 = True
+            break
+        elif run == '2':
+            run2 = True
+            break
+        else:
+            print('неверные данные!')
+    while(run1):
         stroka = int(input("Введите номер строки:"))
         if stroka > 12 or stroka < 1:
             print('Не существующий номер!')
         else:
             break
-    while True:
+    while(run1):
         stolb = int(input("Введите номер столбца:"))
         if stolb > 12 or stolb < 1:
             print('Не существующий номер!')
         else:
             break
+    #================================================
+    while(run2):
+        while(run2):
+            stroka = int(input("Введите номер строки:"))
+            if stroka > 12 or stroka < 1:
+                print('Не существующий номер!')
+            else:
+                break
+        while(run2):
+            stolb = int(input("Введите номер столбца:"))
+            if stolb > 12 or stolb < 1:
+                print('Не существующий номер!')
+            else:
+                break
+        pole[stroka][stolb] = '!'
+        break
     if myLib.pole[stroka-1][stolb-1] == '%':
         print('Игра окончена')
         break
